8e6ef1fb950cb081f11233d63cffad13f75161fe
Erick Ayala <ayala.delgadillo.erick@icloud.com>
2022-05-28 20:00:35 -0500

    first commit

---
+++ PRAC02M07/InternetStatus.swift
@@ -0,0 +1,43 @@
+//
+//  InternetStatus.swift
+//  PRAC02M07
+//
+//  Created by Erick Ayala Delgadillo on 28/05/22.
+//
+import Network
+
+enum InternetType {
+    case none
+    case cellular
+    case wifi
+}
+
+class InternetStatus: NSObject {
+    
+    static let instance = InternetStatus()
+    
+    private override init() {
+        super.init()
+        monitoring()
+    }
+
+    var internetType: InternetType = .none
+    
+    private func monitoring() {
+        let monitor = NWPathMonitor()
+        monitor.pathUpdateHandler = { path in
+            if path.status != .satisfied {
+                self.internetType = .none
+            }
+            else {
+                if path.usesInterfaceType(.wifi) {
+                    self.internetType = .wifi
+                }
+                else if path.usesInterfaceType(.cellular) {
+                    self.internetType = .cellular
+                }
+            }
+        }
+        monitor.start(queue: DispatchQueue.global())
+    }
+}
